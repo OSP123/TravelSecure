64663058a9532db53935a60adeb51f1988171504
Omar Patel <omarpatel123@gmail.com>
2017-12-05 19:57:31 -0800

    convert to proper MVC structure

--- controllers/trips_controller.js
+++ controllers/trips_controller.js
@@ -1,33 +1,26 @@
 var db  = require('../models');
-var express = require('express');
-var router  = express.Router();
-var isAuthenticated = require("../config/middleware/isAuthenticated");
 
-
-router.get('/', isAuthenticated, function(req, res) {
-
-	db.Trip.findAll({
+exports.index = function(req, res) {
+  db.Trip.findAll({
     where: {
-    	UserId: req.user.id
+      UserId: req.user.id
     }
   }).then(function(dbTrip) {
-  	console.log(dbTrip);
+    console.log(dbTrip);
     res.render('trips/trips', {
-  		layout: 'main-trips',
-  		trip: dbTrip
-  	});
+      layout: 'main-trips',
+      trip: dbTrip
+    });
   });
+};
 
-});
+exports.createTrip = function(req, res) {
 
-router.post('/new', isAuthenticated, function(req, res) {
-
-	// Add id from User onto req.body
-	req.body.UserId = req.user.id;
+  // Add id from User onto req.body
+  req.body.UserId = req.user.id;
 
   db.Trip.create(req.body).then(function(dbPost) {
     res.json(dbPost);
   });
-});
+};
 
-module.exports = router;
