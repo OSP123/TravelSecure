f3888a7b9e6a4160cd435ee67bdc7c9938853a27
Omar Patel <omarpatel123@yahoo.com>
2021-01-28 16:08:42 -0800

    Update to es6

--- routes.js
+++ routes.js
@@ -1,4 +1,4 @@
-module.exports = function(app){
+module.exports = app => {
 
 		const application = require('./routes/application');
 		const users = require('./routes/users');
--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.use(compression())
 require('./routes')(app);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   const err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -54,7 +54,7 @@ app.use(function(req, res, next) {
 
 // error handler
 // no stacktraces leaked to user unless in development environment
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
